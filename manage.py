ec6f0dbe82bece3421de8352cc550a5153562c6e
JillHero <jillhero@hotamil.com>
2020-01-23 14:45:50 +0800

    添加配置

--- manage.py
+++ manage.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class Config():
+    debug = True
+
+
+app.config.from_object(Config)
 
 
 
@@ -11,4 +16,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
