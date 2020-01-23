124048782928259bdecbd18c6b2e4c855c9b1c07
JillHero <jillhero@hotamil.com>
2020-01-23 14:50:41 +0800

    配置mysql

--- manage.py
+++ manage.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from pymysql import  install_as_MySQLdb
+install_as_MySQLdb()
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
 
 class Config():
     debug = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:3471515q@127.0.0.1:3306/information_rewiew"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 app.config.from_object(Config)
