233010ddebe9f0a0b37168bd5affb5a6cad86dd1
JillHero <jillhero@hotamil.com>
2020-01-23 14:54:17 +0800

    "添加redis"

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from pymysql import  install_as_MySQLdb
+from redis import StrictRedis
 install_as_MySQLdb()
 
 app = Flask(__name__)
@@ -12,10 +13,13 @@ class Config():
     debug = True
     SQLALCHEMY_DATABASE_URI = "mysql://root:3471515q@127.0.0.1:3306/information_rewiew"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
 
 
 app.config.from_object(Config)
-
+redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 
 
 @app.route("/")
