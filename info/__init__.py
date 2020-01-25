0b5a46aec1f09ca1cd158b32b8de56eb2272dc4c
JillHero <jillhero@hotamil.com>
2020-01-25 17:10:44 +0800

    SQLACHEMY的TEARDOWN操作

--- info/__init__.py
+++ info/__init__.py
@@ -33,7 +33,7 @@ def create_app(config_name):
     db.init_app(app)
     global redis_store
     redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT,decode_responses=True)
-    # CSRFProtect(app)
+    CSRFProtect(app)
     Session(app)
     from info.modules.index import index_blu
 
