f90705f616b97439e3581c2706ac643c1bad3165
JillHero <jillhero@hotamil.com>
2020-01-25 18:09:27 +0800

    新闻分类的后端接口

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import session, render_template, current_app,jsonify
+from flask import session, render_template, current_app, jsonify, request
 
 from . import index_blu
 from ...models import User, News
@@ -24,15 +24,11 @@ def index():
     for news in news_list:
         news_dict_li.append(news.to_basic_dict())
     data = {
-        "user_info":user.to_dict() if user else None,
-        "news_dict_li":news_dict_li
+        "user_info": user.to_dict() if user else None,
+        "news_dict_li": news_dict_li
 
     }
-    return render_template("index.html",data=data)
-
-
-
-
+    return render_template("index.html", data=data)
 
 
 @index_blu.route("/favicon.ico")
@@ -40,3 +36,42 @@ def favicon():
     return current_app.send_static_file("news/favicon.ico")
 
 
+@index_blu.route("/news_list")
+def news_list():
+    cid = request.args.get("cid", "1")
+    page = request.args.get("page", "1")
+    per_page = request.args.get("per_page", "10")
+
+    try:
+        cid = int(cid)
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    filter = []
+    if cid != 1:
+        filter.append(News.category_id == cid)
+
+    try:
+
+        paginate = News.query.filter(*filter).order_by(News.create_time.desc()).paginate(page, per_page, False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据库查询错误")
+    news_list = paginate.items
+    total_page = paginate.pages
+    current_page = paginate.page
+
+    news_dict_li = []
+    for news in news_list:
+        news_dict_li.append(news.to_basic_dict())
+
+    data = {
+        "total_page": total_page,
+        "current_page": current_page,
+        "news_dict_li": news_dict_li
+    }
+
+    return jsonify(errno=RET.OK, errmsg="OK", data=data)
