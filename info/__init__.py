51f6afd1c74c57af7996bd4ac82393aa0e1e7546
JillHero <jillhero@hotamil.com>
2020-01-25 17:42:56 +0800

    实现新闻排行

--- info/__init__.py
+++ info/__init__.py
@@ -9,6 +9,7 @@ from flask_wtf.csrf import generate_csrf
 from redis import StrictRedis
 
 from config import config
+from info.utils.common import do_index_class
 
 db = SQLAlchemy()
 
@@ -36,6 +37,7 @@ def create_app(config_name):
     redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT,decode_responses=True)
     CSRFProtect(app)
     Session(app)
+    app.add_template_filter(do_index_class,"index_class")
     @app.after_request
     def after_resquest(response):
         csrf_token = generate_csrf()
--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,7 @@
 from flask import session, render_template, current_app,jsonify
 
 from . import index_blu
-from ...models import User
+from ...models import User, News
 from ...utils.response_code import RET
 
 
@@ -14,9 +14,18 @@ def index():
             user = User.query.get(user_id)
         except Exception as e:
             current_app.logger.error(e)
+    news_list = []
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(6)
+    except Exception as e:
+        current_app.logger.error(e)
 
+    news_dict_li = []
+    for news in news_list:
+        news_dict_li.append(news.to_basic_dict())
     data = {
-        "user_info":user.to_dict() if user else None
+        "user_info":user.to_dict() if user else None,
+        "news_dict_li":news_dict_li
 
     }
     return render_template("index.html",data=data)
