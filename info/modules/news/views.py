f5be3539d1997511d5de2ca91c00f5914c82d379
JillHero <jillhero@hotamil.com>
2020-01-26 19:06:46 +0800

    收藏和取消收藏的后端逻辑

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,9 +1,10 @@
-from flask import render_template, current_app, session, g, abort
+from flask import render_template, current_app, session, g, abort, request, jsonify
 
 from info import constants
 from info.models import News, User
 from info.modules.news import news_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 
 
 @news_blu.route("/<int:news_id>")
@@ -38,7 +39,10 @@ def news_detail(news_id):
 
     news.clicks +=1
 
-    is_collected = True
+    is_collected = False
+    if user:
+        if news in user.collection_news:
+            is_collected = True
 
 
     data = {
@@ -51,3 +55,48 @@ def news_detail(news_id):
     }
 
     return render_template("news/detail.html", data=data)
+
+
+
+@news_blu.route("/news_collect")
+@user_login_data
+def collect_news():
+
+    user = g.user
+
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登陆")
+    news_id = request.json.get("news_id")
+    action = request.json.get("action")
+    if not all([news_id,action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ["collect","cancel_collect"]:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    try:
+        news_id = int(news_id)
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news = News.query.get(news_id)
+
+    except Exception as e:
+        return jsonify(errno=RET.DBERR, errmsg="数据查询失败")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="为查询到新闻数据")
+
+    if action == "collect":
+
+        if news not in user.collection_news:
+            user.collection_news.append(news)
+
+    else:
+
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+
+    return jsonify(errno=RET.OK, errmsg="成功")
