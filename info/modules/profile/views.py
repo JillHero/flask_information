cbaa8a2401a9679bd29dc0acff2194927c3104fa
JillHero <jillhero@hotamil.com>
2020-01-28 19:04:58 +0800

    修改密码的前后端逻辑

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, g, redirect, request, jsonify, current_app
 
-from . import  profile_blu
+from . import profile_blu
 from ... import db, constants
 from ...utils.common import user_login_data
 from ...utils.image_storage import storage
@@ -10,23 +10,21 @@ from ...utils.response_code import RET
 @profile_blu.route("/info")
 @user_login_data
 def get_user_info():
-
     user = g.user
     if not user:
         return redirect("/")
 
+    data = {"user_info": user.to_dict()}
 
-    data = {"user_info":user.to_dict()}
-
-    return render_template("news/user.html",data=data)
+    return render_template("news/user.html", data=data)
 
 
-@profile_blu.route("/base_info",methods=["GET","POST"])
+@profile_blu.route("/base_info", methods=["GET", "POST"])
 @user_login_data
 def base_info():
     if request.method == "GET":
 
-        return render_template("news/user_base_info.html",data={"user_info":g.user.to_dict()})
+        return render_template("news/user_base_info.html", data={"user_info": g.user.to_dict()})
 
 
     elif request.method == "POST":
@@ -35,11 +33,10 @@ def base_info():
         signature = request.json.get("signature")
         gender = request.json.get("gender")
 
-        if not all([nick_name,signature,gender]):
+        if not all([nick_name, signature, gender]):
             return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
 
-
-        if gender not in ["WOMAN","MAN"]:
+        if gender not in ["WOMAN", "MAN"]:
             return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
 
         user = g.user
@@ -53,11 +50,11 @@ def base_info():
         return jsonify(errno=RET.OK, errmsg="成功")
 
 
-@profile_blu.route("/pic_info",methods=["GET","POST"])
+@profile_blu.route("/pic_info", methods=["GET", "POST"])
 @user_login_data
 def pic_info():
     if request.method == "GET":
-        return render_template("news/user_pic_info.html",data={"user_info":g.user.to_dict()})
+        return render_template("news/user_pic_info.html", data={"user_info": g.user.to_dict()})
 
     try:
         file = request.files.get("avatar")
@@ -72,15 +69,27 @@ def pic_info():
 
     g.user.avatar_url = key
     db.session.commit()
-    return jsonify(errno=RET.OK, errmsg="成功",avatar_url=constants.QINIU_DOMIN_PREFIX+key)
+    return jsonify(errno=RET.OK, errmsg="成功", data={"avatar_url": constants.QINIU_DOMIN_PREFIX + key})
 
 
+@profile_blu.route("/pass_info", methods=["GET", "POST"])
+@user_login_data
+def modify_password():
+    if request.method == "GET":
+        return render_template("news/user_pass_info.html")
 
+    news_password = request.json.get("new_password")
+    old_password = request.json.get("old_password")
 
-
-
-
-
-
-
-
+    if not all([news_password, old_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    user = g.user
+    if not user.check_passowrd(old_password):
+        return jsonify(errno=RET.PWDERR, errmsg="原密码错误")
+    
+    user.password = news_password
+    db.session.commit()
+    return jsonify(errno=RET.OK, errmsg="成功")
+    
+    
+   
