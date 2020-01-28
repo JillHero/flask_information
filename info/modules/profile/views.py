5283648bc809b792a4b1a021ff34c195b947b748
JillHero <jillhero@hotamil.com>
2020-01-28 17:27:24 +0800

    基本资料修改

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,12 +1,14 @@
-from flask import render_template, g, redirect, request
+from flask import render_template, g, redirect, request, jsonify
 
 from . import  profile_blu
+from ... import db
 from ...utils.common import user_login_data
+from ...utils.response_code import RET
 
 
 @profile_blu.route("/info")
 @user_login_data
-def user_info():
+def get_user_info():
 
     user = g.user
     if not user:
@@ -18,11 +20,38 @@ def user_info():
     return render_template("news/user.html",data=data)
 
 
-@profile_blu.route("/user_info",methods=["POST"])
+@profile_blu.route("/base_info",methods=["GET","POST"])
 @user_login_data
 def base_info():
     if request.method == "GET":
 
         return render_template("news/user_base_info.html",data={"user_info":g.user.to_dict()})
+
+
+    elif request.method == "POST":
+
+        nick_name = request.json.get("nick_name")
+        signature = request.json.get("signature")
+        gender = request.json.get("gender")
+
+        if not all([nick_name,signature,gender]):
+            return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+
+        if gender not in ["WOMAN","MAN"]:
+            return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+        user = g.user
+
+        user.signature = signature
+        user.nick_name = nick_name
+        user.gender = gender
+
+        db.session.commit()
+
+        return jsonify(errno=RET.OK, errmsg="成功")
+
+
+
 
 
