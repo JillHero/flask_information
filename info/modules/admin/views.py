852421d1c342c035db925f05b105db47038c7d3f
JillHero <jillhero@hotamil.com>
2020-01-31 16:40:46 +0800

    新闻审核的前后端

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,12 +1,13 @@
 import time
 from datetime import datetime, timedelta
 
-from flask import render_template, request, current_app, session, redirect, url_for, g
+from flask import render_template, request, current_app, session, redirect, url_for, g, jsonify
 
-from info import constants
+from info import constants, db
 from info.modules.admin import admin_blu
 from info.models import User, News
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 
 
 @admin_blu.route("/index")
@@ -190,3 +191,36 @@ def news_review_detail(news_id):
     }
 
     return render_template("admin/news_review_detail.html", data=data)
+
+@admin_blu.route("/news_review_action",methods=["POST"])
+def news_review_action():
+    news_id = request.json.get("news_id")
+    action = request.json.get("action")
+
+    if not all([news_id,action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ("accept","reject"):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询失败")
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="未查询到数据")
+
+    if action == "accept":
+        news.status = 0
+        db.session.commit()
+    
+    else:
+        reason = request.json.get("reason")
+        if not reason:
+            return jsonify(errno=RET.PARAMERR, errmsg="请输入拒绝原因")
+        news.status = -1
+        news.reason = reason
+        db.session.commit()
+    return jsonify(errno=RET.OK, errmsg="成功")
+        
